06eb5a68b8547bd429e97c8c80c695594e458312
unknown <mystifiedthomas@gmail.com>
2018-10-05 08:57:50 -0500

    First stepping stone

---
+++ main.py
@@ -0,0 +1,62 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:Jackson1313@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    entry = db.Column(db.String(120))
+
+    def __init__(self, name, entry):
+        self.name = name
+        self.entry = entry
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return redirect('/Blog')
+
+@app.route('/Blog', methods=['GET', 'POST'])
+def blog():
+    allentries = Blog.query.all()
+    return render_template('Blog.html', entries=allentries)
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def newpost():
+    if request.method == 'POST':
+        errorname = ""
+        errorentry = ""
+        name = request.form['name']
+        if not name:
+            errorname = "Please submit a name for the post"
+        entry = request.form['entry']
+        if not entry:
+            errorentry = "Please submit and entry for the post"
+        if errorname or errorentry:
+            return render_template('newpost.html', errorname = errorname, errorentry = errorentry)
+        else:
+            Blog.name = name
+            Blog.entry = entry
+            new_entry = Blog(name, entry)
+            db.session.add(new_entry)
+            db.session.commit()
+            return redirect("/Blog")
+
+    return render_template('newpost.html', errorname = "", errorentry = "")
+
+        
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
